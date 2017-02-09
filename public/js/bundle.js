b4c8c82afafb3bfa8d8c51ca70da6146d7261fd4
siddharth dubey <dubey.siddharth@outlook.com>
2017-02-10 01:04:45 +0530

    BestOf layout completed

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12191,7 +12191,7 @@ var _SearchHomeLayout = __webpack_require__(118);
 
 var _SearchHomeLayout2 = _interopRequireDefault(_SearchHomeLayout);
 
-var _BestOfLayout = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./components/BestOfLayout\""); e.code = 'MODULE_NOT_FOUND';; throw e; }()));
+var _BestOfLayout = __webpack_require__(113);
 
 var _BestOfLayout2 = _interopRequireDefault(_BestOfLayout);
 
@@ -12224,7 +12224,58 @@ module.exports = __webpack_require__(155);
 
 
 /***/ }),
-/* 113 */,
+/* 113 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(5);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouter = __webpack_require__(34);
+
+var _FeaturedListLayout = __webpack_require__(72);
+
+var _FeaturedListLayout2 = _interopRequireDefault(_FeaturedListLayout);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var BestOfLayout = function (_React$Component) {
+	_inherits(BestOfLayout, _React$Component);
+
+	function BestOfLayout(props) {
+		_classCallCheck(this, BestOfLayout);
+
+		return _possibleConstructorReturn(this, (BestOfLayout.__proto__ || Object.getPrototypeOf(BestOfLayout)).call(this, props));
+	}
+
+	_createClass(BestOfLayout, [{
+		key: 'render',
+		value: function render() {
+			return _react2.default.createElement(_FeaturedListLayout2.default, { type: this.props.params.item });
+		}
+	}]);
+
+	return BestOfLayout;
+}(_react2.default.Component);
+
+exports.default = BestOfLayout;
+
+/***/ }),
 /* 114 */
 /***/ (function(module, exports, __webpack_require__) {
 
